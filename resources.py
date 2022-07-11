df397620a693a672272807e844d38ad6aeca9081
enzo <enzo.ccc@gmail.com>
2022-07-11 08:57:33 +0300

    v4.8

--- resources.py
+++ resources.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created by: The Resource Compiler for PyQt5 (Qt v5.11.2)
+# Created by: The Resource Compiler for PyQt5 (Qt v5.15.2)
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -294,7 +294,7 @@ qt_resource_struct_v2 = b"\
 \x00\x00\x00\x14\x00\x02\x00\x00\x00\x01\x00\x00\x00\x03\
 \x00\x00\x00\x00\x00\x00\x00\x00\
 \x00\x00\x00\x46\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
-\x00\x00\x01\x7b\xe9\xe5\xfa\x7f\
+\x00\x00\x01\x7d\xc8\x88\xc7\x88\
 "
 
 qt_version = [int(v) for v in QtCore.qVersion().split('.')]
